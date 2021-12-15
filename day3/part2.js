02ce00725f427142cb5054e83ba6df01c053f7d6
timvanschie1 <timvanschie1@gmail.com>
2021-12-15 23:06:41 +0100

    Day 3 part 2

--- day3/part2.js
+++ day3/part2.js
@@ -1,42 +1,47 @@
 const getDecimalFromBinaryArray = require("./getDecimalFromBinaryArray.js");
 
 function part2(data) {
-    const positionArray = data[0].split('').map(() => []);
+    function getPositionArrayFromData(data) {
+        let positionArray = data[0].split('').map(() => []);
 
-    for (let i = 0; i < data[0].length; i++) {
-        data.forEach(binary => {
-            positionArray[i] = positionArray[i] + binary[i];
-        })
+        for (let i = 0; i < data[0].length; i++) {
+            data.forEach(binary => {
+                positionArray[i] = positionArray[i] + binary[i];
+            })
+        }
+
+        return positionArray;
     }
 
     let oxyGenRating = [...data];
     let co2ScrubRating = [...data];
 
-    positionArray.forEach((position, i) => {
-        const nrOfZeros = (position.match(/0/g) || []).length;
-        const nrOfOnes = position.length - nrOfZeros;
-        const mostCommonValue = nrOfOnes >= nrOfZeros ? '1' : '0';
-        const leastCommonValue = nrOfOnes >= nrOfZeros ? '0' : '1';
-
+    for (let i = 0; i < data[0].length; i++) {
         if (oxyGenRating.length > 1) {
+            let oxyGenRatingPosArray = getPositionArrayFromData(oxyGenRating);
+            let nrOfZeros = (oxyGenRatingPosArray[i].match(/0/g) || []).length;
+            let nrOfOnes = oxyGenRatingPosArray[i].length - nrOfZeros;
+            let mostCommonValue = nrOfOnes >= nrOfZeros ? '1' : '0';
+
             oxyGenRating = oxyGenRating.filter(binary => {
                 return binary[i] === mostCommonValue;
             });
         }
 
         if (co2ScrubRating.length > 1) {
+            let co2ScrubRatingPosArray = getPositionArrayFromData(co2ScrubRating);
+            let nrOfZeros = (co2ScrubRatingPosArray[i].match(/0/g) || []).length;
+            let nrOfOnes = co2ScrubRatingPosArray[i].length - nrOfZeros;
+            let leastCommonValue = nrOfOnes >= nrOfZeros ? '0' : '1';
+
             co2ScrubRating = co2ScrubRating.filter(binary => {
                 return binary[i] === leastCommonValue;
             });
         }
-    });
-
-    console.log(oxyGenRating);
+    }
 
     const oxyGenBinaryArray = oxyGenRating[0].split('').map(bit => Number(bit));
     const co2ScrubRatingArray = co2ScrubRating[0].split('').map(bit => Number(bit));
-
-    console.log({oxyGenBinaryArray});
 
     return getDecimalFromBinaryArray(oxyGenBinaryArray) * getDecimalFromBinaryArray(co2ScrubRatingArray);
 }
